a1fef5d784901ca119d28e3dd743b57af2368abe
William Shackleton <w.shackleton@gmail.com>
2011-09-09 23:10:10 +0100

    Changed branding, added license to code.

--- src/uk/digitalsquid/ninepatcher/ui/AboutDialog.java
+++ src/uk/digitalsquid/ninepatcher/ui/AboutDialog.java
@@ -1,3 +1,23 @@
+/*
+ * This file is part of Patchworker
+ * Patchworker - easily draw 9-patch images for Android development
+ * Copyright (C) 2011 Will Shackleton
+ *
+ * Patchworker is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Patchworker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Patchworker, in the file COPYING.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package uk.digitalsquid.ninepatcher.ui;
 
 import java.awt.BorderLayout;
@@ -38,7 +58,7 @@ public class AboutDialog extends JDialog implements WindowListener {
 			JPanel titlePanel = new JPanel();
 			titlePanel.setLayout(new BoxLayout(titlePanel, BoxLayout.LINE_AXIS));
 			
-			JLabel label = new JLabel("9patcher");
+			JLabel label = new JLabel("Patchworker");
 			label.setFont(label.getFont().deriveFont(24f));
 			titlePanel.add(label);
 			
